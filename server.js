eb97b0eb6cc57f173ab054c4b8f1e4d6ef5cddb9
DavePach <andersond.checo@gmail.com>
2022-06-12 15:53:04 -0600

    v1

---
+++ server.js
@@ -0,0 +1,53 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json())
+
+app.get('/test', (request, response) => {
+  console.log('solicitud', request)
+  const respuesta = {respuesta1: 'Hola mundo'}
+  response.status(200).json({ respuesta: 'uno', clave2: 2000, nombres: true, holagente: respuesta})
+
+
+})
+
+app.post('/login', (request, response) => {
+  console.log('Datos entrantes',request.body)
+  const email = request.body.email
+  const password = request.body.password
+
+  if (email === undefined || email === ''){
+    response.status(500).json({error: true, message: 'Correo requerido'})
+  }else if(password === undefined || password === ''){
+    response.status(500).json({error: true, message: 'Contraseña requerida'})
+  }else{
+    response.status(200).json({error: false, message: 'Se ingreso correctamente'})
+  }
+})
+
+app.post('/register', (request, response) => {
+  console.log('Datos entrantes', request.body)
+  const name = request.body.name
+  const email = request.body.email
+  const password = request.body.password
+
+  if(name === undefined || name === ''){
+    response.status(500).json({error: true, message: 'Nombre es requerido'})
+  }else if (email === undefined || email === ''){
+    response.status(500).json({error: true, message: 'El correo es requerido'})
+  }else if (password === undefined || password === ''){
+    response.status(500).json({error: true, message: 'La contraseña es requerida'})
+  }else{
+    response.status(200).json({error: false, message: 'Se a registrado con exito'})
+  }
+})
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Aplicacion corriendo en http://localhost:${port}`);
+});
