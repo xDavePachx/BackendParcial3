af3b0ba9180f4468af28ea8fa1c0d2861cbb7912
DavePach <andersond.checo@gmail.com>
2022-06-12 18:39:54 -0600

    Login y resgister listos

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 const db = require('./models');
+const User = db.user;
 
 app.get('/test', (request, response) => {
   console.log('solicitud', request)
@@ -13,21 +14,42 @@ app.get('/test', (request, response) => {
 
 })
 
-app.post('/login', (request, response) => {
+app.post('/login', async (request, response) => {
   console.log('Datos entrantes',request.body)
   const email = request.body.email
   const password = request.body.password
 
   if (email === undefined || email === ''){
     response.status(500).json({error: true, message: 'Correo requerido'})
-  }else if(password === undefined || password === ''){
-    response.status(500).json({error: true, message: 'Contraseña requerida'})
-  }else{
-    response.status(200).json({error: false, message: 'Se ingreso correctamente'})
+    return
   }
+  
+  if(password === undefined || password === ''){
+    response.status(500).json({error: true, message: 'Contraseña requerida'})
+    return
+  }
+
+  const user = await User.findOne({where: {email: email}});
+  console.log('usuario', user) 
+  console.log('email', email)
+  if (user === null){
+    response.status(500).json({error: true, message: 'este usuario no existe '})
+    return
+  }
+
+  if (password !== user.dataValues.password){
+    response.status(500).json({error: true, message: 'la contraseña no es valida' })
+    return
+  }
+  
+  response.status(200).json({error: false, message: 'Se ingreso correctamente'})
+
+
 })
 
-app.post('/register', (request, response) => {
+
+
+app.post('/register', async (request, response) => {
   console.log('Datos entrantes', request.body)
   const name = request.body.name
   const email = request.body.email
@@ -35,13 +57,38 @@ app.post('/register', (request, response) => {
 
   if(name === undefined || name === ''){
     response.status(500).json({error: true, message: 'Nombre es requerido'})
-  }else if (email === undefined || email === ''){
-    response.status(500).json({error: true, message: 'El correo es requerido'})
-  }else if (password === undefined || password === ''){
-    response.status(500).json({error: true, message: 'La contraseña es requerida'})
-  }else{
-    response.status(200).json({error: false, message: 'Se a registrado con exito'})
+    return
   }
+  
+  if (email === undefined || email === ''){
+    response.status(500).json({error: true, message: 'El correo es requerido'})
+    return
+  }
+  
+  if (password === undefined || password === ''){
+    response.status(500).json({error: true, message: 'La contraseña es requerida'})
+    return
+  }
+
+  const user = {
+    name: name,
+    email: email,
+    password: password
+  }
+
+  
+
+  const newUser = await User.create(user);
+  if(newUser === null){
+    response.status(500).json({error: true, message: error})
+    return
+  }
+  console.log('Enviando respuesta exitosa')
+  response.status(200).json({error: false, message: 'Se a registrado con exito', data: data})
+
+  console.log('Si llegamos abajo')
+
+
 })
 
 
